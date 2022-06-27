35090f948cf73b5834a5674226ccf981713870db
Dmitriy Zagaynov <dmz55@ya.ru>
2022-06-27 12:13:07 +0600

    HW 1.3/2

---
+++ 1.3-docker/HW2/stocks_products/logistic/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        for position in positions:
+            position['stock'] = stock
+            StockProduct.objects.create(**position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            # stock_product = StockProduct.objects\
+            #     .filter(stock=instance.id)\
+            #     .filter(product=position['product'].id)
+            position['stock'] = stock
+            # stock_product.update_or_create(**position)
+
+            StockProduct.objects.update_or_create(
+                stock=instance.id, product=position['product'].id,
+                defaults={**position},
+            )
+
+        return stock
